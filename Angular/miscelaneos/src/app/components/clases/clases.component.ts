f47f0c13b3a37a6d60dc4011ff57b3cd0a6e480f
jaquicenoo <36316958+jaquicenoo@users.noreply.github.com>
2019-02-17 19:42:00 -0500

    boton tipo floting label

--- Angular/miscelaneos/src/app/components/clases/clases.component.ts
+++ Angular/miscelaneos/src/app/components/clases/clases.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-clases',
   templateUrl: './clases.component.html',
-  styles: []
+  styleUrls: ['./stylos.css']
 })
 export class ClasesComponent implements OnInit {
 
@@ -19,6 +19,20 @@ export class ClasesComponent implements OnInit {
   ejecutar() {
     this.loading = true;
     setTimeout(() => this.loading = false, 3000);
+  }
+  change($event) {
+    if ($event.target.value !== 'value1') {
+      $event.target.classList.remove('select');
+    } else {
+      $event.target.classList.add('select');
+    }
+  }
+  blur($event) {
+    if ($event.target.value.length > 3) {
+      $event.target.classList.add('invalid');
+    } else {
+      $event.target.classList.remove('invalid');
+    }
   }
 
 }
